6317aac03ad2cf5ae26f2c1f752d01a159efa0d9
mimiqkz <npq1@hi.is>
2017-09-18 19:39:56 +0000

    Update the models

--- src/main/java/teymi15/model/Ingredient.java
+++ src/main/java/teymi15/model/Ingredient.java
@@ -5,5 +5,57 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 public class Ingredient {
+    private int ID;
+    private double price; //Using different currency
+    private String name; //Name of the ingredient e.g cherry tomato, apple etc.
+    private String location;
+    private String store;
 
+    public Ingredient(int id, double price, String name, String location, String store) {
+        this.ID = id;
+        this.price = price;
+        this.name = name;
+        this.location = location;
+        this.store = store;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public void setStore(String store) {
+        this.store = store;
+    }
 }
--- src/main/java/teymi15/model/Recipe.java
+++ src/main/java/teymi15/model/Recipe.java
@@ -5,5 +5,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 public class Recipe {
+    private int ID;
+    private String name;
+    private String instruction;
+    private Ingredient[] ingredients;
 
+    public Recipe(int ID, String name, String instruction, Ingredient[] in) {
+        this.ID = ID;
+        this.name = name;
+        this.instruction = instruction;
+        this.ingredients = in;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public Ingredient[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Ingredient[] ingredients) {
+        this.ingredients = ingredients;
+    }
 }
