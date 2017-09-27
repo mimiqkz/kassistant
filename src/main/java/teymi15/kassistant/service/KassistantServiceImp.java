efa69bfcbf1f2abab22a37e961299f6aaacdd552
zurgur <harryfreyr@gmail.com>
2017-09-27 20:32:38 +0000

    opens the aws server and makes tables for recipe and Ingredient. (Ingredient an recipe not connected together yet) :)

--- src/main/java/teymi15/kassistant/service/KassistantServiceImp.java
+++ src/main/java/teymi15/kassistant/service/KassistantServiceImp.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class RecipeServiceImp implements RecipeService{
+public class KassistantServiceImp implements KassistantService{
 
 
 
