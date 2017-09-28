e04a3d0d7edd39fd4e0cdfaf448716531e7a7856
meatyminx <flexayoung@gmail.com>
2017-09-28 14:04:14 +0200

    Added User object
    Created basic skeleton for user object so we can start working with it

---
+++ src/main/java/teymi15/kassistant/model/User.java
@@ -0,0 +1,57 @@
+package teymi15.kassistant.model;
+
+/**
+ * The program allows user to search for recipe with the matching name.
+ * For example, "Apple pie", "Brocoli soup", "Chocolate Cake" , etc.
+ *
+ * @author Nu Phan Quynh Do
+ * @author  Alexander Freyr Sveinsson
+ * @author Alexandra Mjöll Young
+ * @version 1.0
+ * @since   2017-09-20
+ */
+
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String userName) {
+        this.username = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "User[id=%d, username='%s', password = '%s']",
+                id, username, password);
+    }
+}
