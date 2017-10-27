b95c3300db019b83bbabb76c1f7a8f07e8819a6c
meatyminx <flexayoung@gmail.com>
2017-10-27 14:07:47 +0200

    Minor style changes
    Error message bootstrapped - button details

--- src/main/resources/static/scripts/navbar.js
+++ src/main/resources/static/scripts/navbar.js
@@ -1,3 +1,9 @@
+$(document).ready(function() {
+    $('.alert-danger').hide();
+
+});
+
+
 $('ul.nav li.dropdown').hover(function() {
     $(this).find('.dropdown-menu-user').stop(true, true).delay(200).fadeIn(500);
 }, function() {
@@ -5,7 +11,7 @@ $('ul.nav li.dropdown').hover(function() {
 });
 
 
-//Form Submit
+//Form Submit stuff
 function sendData()
 {
     $.ajax(
@@ -16,11 +22,12 @@ function sendData()
             url: "/sign-up",
             success: function(data, textStatus, jqXHR)
             {
+                $(".alert-danger").hide();
                 window.location.reload();
             },
             error: function(jqXHR)
             {
-
+                $(".alert-danger").show();
                 $(".error-message").text(jqXHR.responseText);
             }
 
