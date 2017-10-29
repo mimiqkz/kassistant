15fb3ff80c174712b556e5fdba79fb519e59c93d
zurgur <harryfreyr@gmail.com>
2017-10-29 14:28:21 +0000

    WebLayerTest

--- src/test/java/teymi15/kassistant/WebLayerTest.java
+++ src/test/java/teymi15/kassistant/WebLayerTest.java
@@ -3,41 +3,55 @@ package teymi15.kassistant;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import teymi15.kassistant.control.IngredientController;
+import teymi15.kassistant.service.IngredientService;
+import teymi15.kassistant.service.IngredientServiceImp;
+import teymi15.kassistant.service.PhotoServiceImp;
+import teymi15.kassistant.service.UserServiceImp;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 /**
  *
  * @author Alexander Freyr Sveinsson
  * @date október 2017
  * HBV501G Hugbúnaðarverkefni 1 Háskóli Íslands
  *
- * a test class that performs tests without calling the server
+ * Prófunarklasi sem framkvæmir prófanir á weblayer og notar WebMvcTest
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-public class ApplicationTest {
-    // Server (Tomcat) not made to run
+/**
+ *  only the specified class is run not the hole context
+ *  we aske to only runn the IngredientController class
+ */
+@WebMvcTest(IngredientController.class)
+public class WebLayerTest {
+
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    IngredientServiceImp ingredientService;
+    @MockBean
+    PhotoServiceImp photoService;
+    @MockBean
+    UserServiceImp userService;
     /**
-     * Class to test if it works to send a HttpRequest to /create-ingredient
-     * and to see if we get back createIngredient.html page that has the String name
+     * class that teks to run HttpRequest on /create-ingredient
+     * and get back the CreateIngredient.html page
      */
     @Test
     public void newIngretientretReturnsName() throws Exception {
         this.mockMvc.perform(get("/create-ingredient"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("name")));
-
     }
-
 }
