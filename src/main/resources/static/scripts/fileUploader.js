4aeb7e2b206219494b114bb211494c419d7b9761
zurgur <harryfreyr@gmail.com>
2017-11-15 14:25:11 +0000

    jqurey test

--- src/main/resources/static/scripts/fileUploader.js
+++ src/main/resources/static/scripts/fileUploader.js
@@ -1,7 +1,6 @@
-$(document).on('change', ':file', function() {
+$(document).ready( function() {
     var input = $(this);
     var label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
     input.trigger('fileselect', label);
     $('#file-output').val(label);
 });
-
